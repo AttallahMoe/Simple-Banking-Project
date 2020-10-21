60ea7591d802f697113e6961874797983e48fce2
Mohammad M Attallah <ma2235@afsconnect1.njit.edu>
2020-10-20 22:46:04 -0400

    updated logout

--- project/logout.php
+++ project/logout.php
@@ -1,7 +1,6 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+  
 <?php
-//since this function call is included we can omit it here. Having multiple calls to session_start() will cause errors/warnings
-//session_start();
+session_start();
 // remove all session variables
 session_unset();
 // destroy the session

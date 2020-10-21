6b90e24c48282a8654d3a2ccdb8903ef7ec7b2e0
Mohammad M Attallah <ma2235@afsconnect1.njit.edu>
2020-10-20 22:23:51 -0400

    test

--- project/login.php
+++ project/login.php
@@ -1,63 +1,78 @@
+
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <form method="POST">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required/>
-    <label for="p1">Password:</label>
-    <input type="password" id="p1" name="password" required/>
-    <label for="p2">Confirm Password:</label>
-    <input type="password" id="p2" name="confirm" required/>
-    <input type="submit" name="register" value="Register"/>
+    <input type="email" id="email" placeholder="Email" name="email" required/>
+    <input type="password" id="p1" placeholder="Password" name="password" required/>
+    <input type="submit" name="login" value="Login"/>
 </form>
+
 <?php
-if (isset($_POST["register"])) {
+if (isset($_POST["login"])) {
     $email = null;
     $password = null;
-    $confirm = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
     }
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
     }
-    if (isset($_POST["confirm"])) {
-        $confirm = $_POST["confirm"];
-    }
     $isValid = true;
-    //check if passwords match on the server side
-    if ($password == $confirm) {
-        echo "Passwords match <br>";
-    }
-    else {
-        echo "Passwords don't match<br>";
+    if (!isset($email) || !isset($password)) {
         $isValid = false;
+        flash("Email or password missing");
     }
-    if (!isset($email) || !isset($password) || !isset($confirm)) {
+    if (!strpos($email, "@")) {
         $isValid = false;
+        //echo "<br>Invalid email<br>";
+        flash("Invalid email");
     }
-    //TODO other validation as desired, remember this is the last line of defense
     if ($isValid) {
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        
         $db = getDB();
         if (isset($db)) {
-            //here we'll use placeholders to let PDO map and sanitize our data
-            $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
-            //here's the data map for the parameter to data
-            $params = array(":email" => $email, ":password" => $hash);
+            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
+
+            $params = array(":email" => $email);
             $r = $stmt->execute($params);
-            //let's just see what's returned
-            echo "db returned: " . var_export($r, true);
+            //echo "db returned: " . var_export($r, true);
             $e = $stmt->errorInfo();
-            if ($e[0] == "00000") {
-                echo "<br>Welcome! You successfully registered, please login.";
+            if ($e[0] != "00000") {
+                //echo "uh oh something went wrong: " . var_export($e, true);
+                flash("Something went wrong, please try again");
+            }
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result && isset($result["password"])) {
+                $password_hash_from_db = $result["password"];
+                if (password_verify($password, $password_hash_from_db)) {
+                    $stmt = $db->prepare("
+SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
+                    $stmt->execute([":user_id" => $result["id"]]);
+                    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    unset($result["password"]);//remove password so we don't leak it beyond this page
+                    //let's create a session for our user based on the other data we pulled from the table
+                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
+                    if ($roles) {
+                        $_SESSION["user"]["roles"] = $roles;
+                    }
+                    else {
+                        $_SESSION["user"]["roles"] = [];
+                    }
+                    //on successful login let's serve-side redirect the user to the home page.
+                    flash("Log in successful");
+                    die(header("Location: home.php"));
+                }
+                else {
+                    flash("Invalid password");
+                }
             }
             else {
-                echo "uh oh something went wrong: " . var_export($e, true);
+                flash("Invalid user");
             }
         }
     }
     else {
-        echo "There was a validation issue";
+        flash("There was a validation issue");
     }
 }
 ?>
+<?php require(__DIR__ . "/partials/flash.php");
--- project/logout.php
+++ project/logout.php
@@ -6,6 +6,11 @@
 session_unset();
 // destroy the session
 session_destroy();
-echo "You're logged out (proof by dumping the session)<br>";
-echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 ?>
+<?php require_once(__DIR__ . "/partials/nav.php");/*ultimately, this is just here for the function to be loaded now*/ ?>
+<?php
+//flash("You have been logged out");
+die(header("Location: login.php"));
+?>
+
+echo "You have been logged out<br>";

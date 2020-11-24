264f9f60874fa5b4a335535c0382dfc2cc51f56e
AttallahMoe <attallahmoe@gmail.com>
2020-11-23 19:36:21 -0500

    added links to home page

--- project/home.php
+++ project/home.php
@@ -8,4 +8,13 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 ?>
 <p>Welcome, <?php echo $email; ?></p>
 
+<div class="list-group">
+    <div>
+        <a href="createAccount.php">Create Account</a>
+        <a href="viewAccount.php">View Accounts</a>
+        <a href="createTransaction.php">Personal Transaction</a>
+        <a href="createPersonalTransfer.php">Personal Transfer</a>
+    </div>
+</div>
+
 <?php require(__DIR__ . "/partials/flash.php"); ?>

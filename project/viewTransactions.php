e5aa43ad93841c5df7fbe70c23036d89c597e7ec
attallahmoe <attallahmoe@gmail.com>
2020-11-27 14:44:34 -0500

    update

--- project/viewTransactions.php
+++ project/viewTransactions.php
@@ -118,6 +118,11 @@ if($check) {
         $type = $_POST["action_type"];
         $results = [];
 
+        $startDate = (String)$startDate . ' 00:00:00';
+        $endDate = (String)$endDate . ' 00:00:00';
+
+        echo $endDate;
+
         $stmt = $db->prepare("SELECT act_src_id, Accounts.id, Accounts.account_number, amount, action_type, memo FROM Transactions JOIN Accounts on Accounts.id = Transactions.act_dest_id WHERE act_src_id =:id AND action_type=:action_type AND CAST(created AS DATE) BETWEEN startDate=:startDate AND endDate=:endDate LIMIT 10");
         $r = $stmt->execute([":id" => $transId, ":action_type" => $type, ":startDate" => $startDate, ":endDate" => $endDate]);
         if ($r){

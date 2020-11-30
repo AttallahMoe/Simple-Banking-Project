d2194231ee3d5b0b071d3a1ea3cabe44b6ba9ae6
AttallahMoe <attallahmoe@gmail.com>
2020-11-30 13:55:27 -0500

    update

--- project/viewTransactions.php
+++ project/viewTransactions.php
@@ -34,7 +34,7 @@ if($check) {
     $db = getDB();
 
     //TODO pageination
-    $numPerPage = 1;
+    $numPerPage = 5;
     $numRecords = 0;
 
     $stmt = $db->prepare("SELECT COUNT(act_src_id) FROM Transactions WHERE id=:id");

ab6b103d42894e026d7fbdac521bb037dd498a43
AttallahMoe <attallahmoe@gmail.com>
2020-11-30 13:50:22 -0500

    update

--- project/viewTransactions.php
+++ project/viewTransactions.php
@@ -168,6 +168,21 @@ require(__DIR__ . "/partials/flash.php");
                                 <div><?php safer_echo($r["memo"]); ?></div>
                             </div>
                         </div>
+                </div>
+        </div>
+        <nav aria-label="Filtered">
+            <ul class="pagination justify-content-center">
+                <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+                    <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+                </li>
+                <?php for($i = 0; $i < $numLinks; $i++):?>
+                    <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo ($page+1) >= $numLinks?"disabled":"";?>">
+                    <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page+1;?>">Next</a>
+                </li>
+            </ul>
+        </nav>
                     <?php endforeach; ?>
                 </div>
             <?php else: ?>
@@ -175,17 +190,4 @@ require(__DIR__ . "/partials/flash.php");
             <?php endif; ?>
             </div>
             </div>
-            <nav aria-label="Filtered">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-                        <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
-                    </li>
-                    <?php for($i = 0; $i < $numLinks; $i++):?>
-                        <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
-                    <?php endfor; ?>
-                    <li class="page-item <?php echo ($page+1) >= $numLinks?"disabled":"";?>">
-                        <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page+1;?>">Next</a>
-                    </li>
-                </ul>
-            </nav>
         </div>

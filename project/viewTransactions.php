62b48c7b0475c653d9fb5c2aa10e66fb0c2a9605
AttallahMoe <attallahmoe@gmail.com>
2020-12-03 12:15:42 -0500

    test

--- project/viewTransactions.php
+++ project/viewTransactions.php
@@ -34,7 +34,7 @@ if($check) {
     $db = getDB();
 
     //TODO pageination
-    $numPerPage = 5;
+    $numPerPage = 1;
     $numRecords = 0;
 
     $stmt = $db->prepare("SELECT COUNT(act_src_id) FROM Transactions WHERE id=:id");
@@ -152,21 +152,6 @@ require(__DIR__ . "/partials/flash.php");
             <?php if(count($results) > 0 && isset($_POST["save"])): ?>
                 <h1><strong>Filtered Transactions</strong></h1>
                 <div class="list-group">
-                </div>
-        </div>
-        <nav aria-label="Filtered">
-            <ul class="pagination justify-content-center">
-                <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-                    <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
-                </li>
-                <?php for($i = 0; $i < $numLinks; $i++):?>
-                    <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
-                <?php endfor; ?>
-                <li class="page-item <?php echo ($page+1) >= $numLinks?"disabled":"";?>">
-                    <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page+1;?>">Next</a>
-                </li>
-            </ul>
-        </nav>
                     <?php foreach ($results as $r): ?>
                         <div class="list-group-item">
                             <div>
@@ -191,5 +176,20 @@ require(__DIR__ . "/partials/flash.php");
             <?php else: ?>
                 <p>No Results</p>
             <?php endif; ?>
+        </div>
+    </div>
+<nav aria-label="Filtered">
+    <ul class="pagination justify-content-center">
+        <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+            <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+        </li>
+        <?php for($i = 0; $i < $numLinks; $i++):?>
+            <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+        <?php endfor; ?>
+        <li class="page-item <?php echo ($page+1) >= $numLinks?"disabled":"";?>">
+            <a class="page-link" href="?id=<?php echo $transId;?>?page=<?php echo $page+1;?>">Next</a>
+        </li>
+    </ul>
+</nav>
 
         </div>
